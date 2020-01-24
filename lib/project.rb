96a96d3586fed1da555dd1d5fc4c9b3a868f5a6f
Austin Johnson <technicallyaustin@gmail.com>
2020-01-24 18:45:31 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,11 @@
 class Project
+  @@all = [] 
   attr_accessor :backer, :backers 
   attr_reader :backed_projects, :title 
 def initialize(title)
   @title = title
   @backers = []
+  @@all << self 
 end
 
 def add_backer(backer)
